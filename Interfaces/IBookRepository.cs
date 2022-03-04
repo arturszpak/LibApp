2c200eee324a31067bcf7a609d9e64a991934a33
Artur Szpak <artur.szpak1@gmail.com>
2022-03-04 22:53:20 +0100

    3

--- Interfaces/IBookRepository.cs
+++ Interfaces/IBookRepository.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LibApp.Models;
 
 namespace LibApp.Interfaces
 {
-    interface IBookRepository
+    public interface IBookRepository
     {
+        IEnumerable<Book> GetBooks();
+        Book GetBookById(int id);
+        public void AddBook(Book book);
+        public void UpdateBook(Book book);
+        public void DeleteBook(int id);
+        public void Save();
     }
 }
---
+++ Repositories/BookRepository.cs
@@ -0,0 +1,29 @@
+﻿using LibApp.Interfaces;
+using LibApp.Data;
+using LibApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibApp.Repositories 
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public BookRepository( ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Book> GetBooks()
+        {
+            return _context.Books;
+        }
+        public Book GetBookById(int id) => _context.Books.Find(id);
+        public void AddBook(Book book) => _context.Books.Add(book);
+        public void UpdateBook(Book book) => _context.Books.Update(book);
+        public void DeleteBook(int id) => _context.Books.Remove(GetBookById(id));
+        public void Save() => _context.SaveChanges();
+    }
+}
