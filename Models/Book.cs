38cb2b33b3c54c14b53d5acb7cf25b65927f766f
Artur Szpak <artur.szpak1@gmail.com>
2022-03-04 22:40:22 +0100

    2

--- Models/Book.cs
+++ Models/Book.cs
@@ -21,9 +21,13 @@ namespace LibApp.Models
 		public byte GenreId { get; set; }
 		public DateTime DateAdded { get; set; }
 		[Display(Name="Release Date")]
+		[Required(ErrorMessage = "ReleaseDate is required.")]
 		public DateTime ReleaseDate { get; set; }
 		[Display(Name = "Number in Stock")]
+		[Required(ErrorMessage = "NumberInStock is required.")]
+		[Range(1, 20, ErrorMessage = "Range must be between 1 and 20.")]
 		public int NumberInStock { get; set; }
+		[Required(ErrorMessage = "NumberAvailable is required.")]
 		public int NumberAvailable { get; set; }
 	}
       
