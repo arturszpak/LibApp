e100635591298d9937648a781b010fd739e2b209
Artur Szpak <artur.szpak1@gmail.com>
2022-03-05 20:30:19 +0100

    tasks 4-6

--- Controllers/Api/BooksController.cs
+++ Controllers/Api/BooksController.cs
@@ -8,21 +8,31 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.EntityFrameworkCore;
 
 namespace LibApp.Controllers.Api
 {
-    [Route("api/[controller]")]
+    [Microsoft.AspNetCore.Mvc.Route("api/[controller]")]
     [ApiController]
-    public class BooksController : ControllerBase
-    {
-        public BooksController(ApplicationDbContext context, IMapper mapper)
+    public class BooksController : ControllerBase { 
+
+        public BooksController( ApplicationDbContext context, IMapper mapper)
         {
             _context = context;
             _mapper = mapper;
         }
 
-        // GET api/books/
-        [HttpGet]
+        [System.Web.Http.HttpGet]
+        [Authorize(Roles = "User")]
+        [Authorize(Roles = "Manager")]
+        public IActionResult GetBooks()
+        {
+            var books = _context.Books.Include(b => b.Genre).ToList().Select(_mapper.Map<Book, BookDto>);
+            return Ok(books);
+        }
+
+
         public IEnumerable<BookDto> GetBooks(string query = null)
         {
             var booksQuery = _context.Books.Where(b => b.NumberAvailable > 0);
